7eaa6f9b0936d52b5cfa6bfd56c48732f1d4151f
João Eduardo <joaoeduardoj920@gmail.com>
2023-06-13 12:42:32 -0300

    feat(express): adding config

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("typescript initial");
+import express from "express";
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.status(200).send("tudo ok");
+});
+
+const port = 3333;
+
+server.listen(port, () => console.log(`Server listening port ${port}`));
