a64e5e00bfc2cc7503b63830043a6dbaab086f2e
João Eduardo <joaoeduardoj920@gmail.com>
2023-06-14 11:57:36 -0300

    refactory(jest): ajust coverageFrom

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,11 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["src/openai/*.ts", "src/services/**/*.ts"],
+  collectCoverageFrom: [
+    "src/openai/*.ts",
+    "src/services/**/*.ts",
+    "src/repositories/*.ts",
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
