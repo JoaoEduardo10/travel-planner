d2012ad7f1e157ae5b39d20c19b7bb197f536578
João Eduardo <joaoeduardoj920@gmail.com>
2023-06-14 11:56:53 -0300

    feat(TravelRepository): adding test

---
+++ tests/repositories/create-travel.test.ts
@@ -0,0 +1,27 @@
+import { Openai } from "../../src/openai/openai";
+import { TravelRepository } from "../../src/repositories/create-travel";
+
+describe("TravelRepository", () => {
+  it("should return trip text promise", () => {
+    const openai = new Openai();
+
+    const travelRepository = new TravelRepository(openai);
+
+    const {
+      best_way_travelPromise,
+      travel_itineraryPromise,
+      violence_infoPromise,
+      weatherPromise,
+    } = travelRepository.getTravelTextPromise({
+      destination: "são paulo-sp",
+      end_date: "20/11/2022",
+      origin: "teresina-pi",
+      start_date: "10/11/2022",
+    });
+
+    expect(best_way_travelPromise).toBeTruthy();
+    expect(travel_itineraryPromise).toBeTruthy();
+    expect(violence_infoPromise).toBeTruthy();
+    expect(weatherPromise).toBeTruthy();
+  });
+});
