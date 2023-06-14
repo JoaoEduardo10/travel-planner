dfbbe7b7f409e7a3065c0e895ff39a310dcdd489
João Eduardo <joaoeduardoj920@gmail.com>
2023-06-14 12:00:54 -0300

    feat(TravelController): created class

--- src/controllers/create-travel.ts
+++ src/controllers/create-travel.ts
@@ -1,63 +1,39 @@
-import { Request, Response } from "express";
+import { Request } from "express";
 import { TravelDTO } from "../interface/travelDTO";
-import { CControllers } from "./protcols";
-import { BestWayTravel } from "../services/best_way_travel/best_way_travel";
-import { Travel_itinerary } from "../services/travel_itinerary/travel-itinerary";
-import { Violence_info } from "../services/violence_info/violence-info";
-import { Weather } from "../services/weather/weather";
-import { Openai } from "../openai/openai";
+import { CControllers, MessageBodyDTO, ResponseDTO } from "./protcols";
+import { ITravelRepository } from "../repositories/protocols";
 
 export class TravelController implements CControllers {
-  async handle(req: Request<{}, {}, TravelDTO>, res: Response): Promise<void> {
-    const { destination, end_date, origin, start_date } = req.body;
+  constructor(private readonly createTravelRepositor: ITravelRepository) {}
 
-    const openaiClient = new Openai();
-    const Best_way_travelText = new BestWayTravel(openaiClient);
-    const Travel_itineraryText = new Travel_itinerary(openaiClient);
-    const WeatherResponseText = new Weather(openaiClient);
-    const Violence_infoText = new Violence_info(openaiClient);
+  async handle(
+    req: Request<{}, {}, TravelDTO>
+  ): Promise<ResponseDTO<MessageBodyDTO>> {
+    const {
+      best_way_travelPromise,
+      travel_itineraryPromise,
+      violence_infoPromise,
+      weatherPromise,
+    } = this.createTravelRepositor.getTravelTextPromise(req.body);
 
-    const best_way_travel = Best_way_travelText.getBestWayTravel({
-      destination,
-      origin,
-      start_date,
-    });
+    const [best_way_travel, travel_itinerary, weather, violence_info] =
+      await Promise.all([
+        best_way_travelPromise,
+        travel_itineraryPromise,
+        weatherPromise,
+        violence_infoPromise,
+      ]);
 
-    const travel_itinerary = Travel_itineraryText.getTravel_itinerary({
-      destination,
-      end_date,
-      start_date,
-    });
-
-    const violence_info = Violence_infoText.getViolence_info({
-      destination,
-      start_date,
-    });
-
-    const weather = WeatherResponseText.getWeather({
-      destination,
-      start_date,
-    });
-
-    const [
-      best_way_travelResponse,
-      travel_itineraryResponse,
-      weatherResponse,
-      violence_infoResponse,
-    ] = await Promise.all([
+    const message = {
       best_way_travel,
       travel_itinerary,
       weather,
       violence_info,
-    ]);
-
-    const message = {
-      best_way_travel: best_way_travelResponse,
-      travel_itinerary: travel_itineraryResponse,
-      weather: weatherResponse,
-      violence_info: violence_infoResponse,
     };
 
-    res.status(201).json(message);
+    return {
+      body: message,
+      statusCode: 201,
+    };
   }
 }
--- src/controllers/protcols.ts
+++ src/controllers/protcols.ts
@@ -1,6 +1,21 @@
 import { Request, Response } from "express";
 import { TravelDTO } from "../interface/travelDTO";
 
+export interface MessageBodyDTO {
+  best_way_travel: string;
+  travel_itinerary: string;
+  weather: string;
+  violence_info: string;
+}
+
+export interface ResponseDTO<B> {
+  body: B;
+  statusCode: number;
+}
+
 export interface CControllers {
-  handle(req: Request<{}, {}, TravelDTO>, res: Response): Promise<void>;
+  handle(
+    req: Request<{}, {}, TravelDTO>,
+    res: Response
+  ): Promise<ResponseDTO<MessageBodyDTO>>;
 }
