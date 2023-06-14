5d563e00d8c3916d988bb8ac2000cba3845f3127
João Eduardo <joaoeduardoj920@gmail.com>
2023-06-14 12:37:07 -0300

    feat(Travel): adding middleware of error

---
+++ src/middlewares/travel/travel-middleware.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from "express";
+import { TravelDTO } from "../../interface/travelDTO";
+import { Bad_Request } from "../../errors/api-errors";
+
+export const travelMiddleware: RequestHandler<{}, {}, TravelDTO> = async (
+  req,
+  _res,
+  next
+) => {
+  const { destination, end_date, origin, start_date } = req.body;
+
+  if (!destination) {
+    throw new Bad_Request("Adicione um destino!");
+  }
+
+  if (!origin) {
+    throw new Bad_Request("Adicione uma origem");
+  }
+
+  if (!start_date) {
+    throw new Bad_Request("Adicione a data de partida");
+  }
+
+  if (!end_date) {
+    throw new Bad_Request("Adicione a data de chegada");
+  }
+
+  next();
+};
