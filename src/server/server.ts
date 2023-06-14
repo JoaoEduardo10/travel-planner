9a66fe1d7c50ddc902a28cd160b2cd5b90510912
João Eduardo <joaoeduardoj920@gmail.com>
2023-06-14 15:44:47 -0300

    feat(swagger): documenting the api with swagger end adding cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,9 +3,19 @@ import express from "express";
 import "dotenv/config";
 import { globalsErrors } from "./middlewares/global-errors";
 import { router } from "./router";
+import swagger from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
+import cors from "cors";
 
 const server = express();
 server.use(express.json());
+server.use(cors());
+
+server.use(
+  "/travel-planner/documentation",
+  swagger.serve,
+  swagger.setup(swaggerDocs)
+);
 
 server.use(router);
 server.use(globalsErrors);
